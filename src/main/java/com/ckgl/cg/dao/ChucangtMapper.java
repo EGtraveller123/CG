e2b7afc0ec7727fd1746e71a98452c28ad713c32
phantomSnail <710312387@qq.com>
2019-05-08 02:04:48 +0800

    不想改了

--- src/main/java/com/ckgl/cg/dao/ChucangtMapper.java
+++ src/main/java/com/ckgl/cg/dao/ChucangtMapper.java
@@ -12,16 +12,16 @@ import java.util.Map;
 @Component
 public interface ChucangtMapper {
 
-    @Select("select k.kuanhao,k.yanse,j.ccriqi,j.xs,j.s,j.m,j.l,j.xl,j.xxl,j.xxxl from jincangt j join kucun_t t on j.kucunid=k.id")
+    @Select("select k.kuanhao,k.yanse,j.ccriqi,j.xs,j.s,j.m,j.l,j.xl,j.xxl,j.xxxl,j.beizhu from chucang_t j join kucun_t k on j.kucunid=k.id")
     List<Map> selectAll();
 
     @Insert("insert into chucang_t(kucunid,ccriqi,xs,s,m,l,xl,xxl,xxxl,beizhu) values (#{kucunid},#{ccriqi},#{xs},#{s},#{m},#{l},#{xl},#{xxl},#{xxxl},#{beizhu})")
     boolean insert(Chucangt chucangt);
 
-    @Select("select k.kuanhao,k.yanse,j.ccriqi,j.xs,j.s,j.m,j.l,j.xl,j.xxl,j.xxxl from chucang_t j join kucun_t k on j.kucunid=k.id where k.kuanhao=#{kuanhao}")
+    @Select("select k.kuanhao,k.yanse,j.ccriqi,j.xs,j.s,j.m,j.l,j.xl,j.xxl,j.xxxl,j.beizhu from chucang_t j join kucun_t k on j.kucunid=k.id where k.kuanhao=#{kuanhao}")
     List<Map> selectByKuanhao(String kuanhao);
 
-    @Select("select k.kuanhao,k.yanse,j.ccriqi,j.xs,j.s,j.m,j.l,j.xl,j.xxl,j.xxxl from chucang_t j join kucun_t k on j.kucunid=k.id where k.kuanhao=#{kuanhao} and k.yanse=#{yanse}")
+    @Select("select k.kuanhao,k.yanse,j.ccriqi,j.xs,j.s,j.m,j.l,j.xl,j.xxl,j.xxxl,j.beizhu from chucang_t j join kucun_t k on j.kucunid=k.id where k.kuanhao=#{kuanhao} and k.yanse=#{yanse}")
     List<Map> findByKuanhaoYanse(String kuanhao,String yanse);
 
 
--- src/main/java/com/ckgl/cg/dao/JincangtMapper.java
+++ src/main/java/com/ckgl/cg/dao/JincangtMapper.java
@@ -9,15 +9,15 @@ import java.util.Map;
 
 public interface JincangtMapper {
 
-    @Select("select k.kuanhao,k.yanse,j.jcriqi,j.xs,j.s,j.m,j.l,j.xl,j.xxl,j.xxxl from jincangt j join kucun_t t on j.kucunid=k.id")
+    @Select("select k.kuanhao,k.yanse,j.jcriqi,j.xs,j.s,j.m,j.l,j.xl,j.xxl,j.xxxl,j.beizhu from jincang_t j join kucun_t k on j.kucunid=k.id")
     List<Map> selectAll();
 
     @Insert("insert into jincang_t(kucunid,jcriqi,xs,s,m,l,xl,xxl,xxxl,beizhu) values (#{kucunid},#{jcriqi},#{xs},#{s},#{m},#{l},#{xl},#{xxl},#{xxxl},#{beizhu})")
     boolean insert(Jincangt jincangt);
 
-    @Select("select k.kuanhao,k.yanse,j.jcriqi,j.xs,j.s,j.m,j.l,j.xl,j.xxl,j.xxxl from jincang_t j join kucun_t k on j.kucunid=k.id where k.kuanhao=#{kuanhao}")
+    @Select("select k.kuanhao,k.yanse,j.jcriqi,j.xs,j.s,j.m,j.l,j.xl,j.xxl,j.xxxl,j.beizhu from jincang_t j join kucun_t k on j.kucunid=k.id where k.kuanhao=#{kuanhao}")
     List<Map> selectByKuanhao(String kuanhao);
 
-    @Select("select k.kuanhao,k.yanse,j.jcriqi,j.xs,j.s,j.m,j.l,j.xl,j.xxl,j.xxxl from jincang_t j join kucun_t k on j.kucunid=k.id where k.kuanhao=#{kuanhao} and k.yanse=#{yanse}")
+    @Select("select k.kuanhao,k.yanse,j.jcriqi,j.xs,j.s,j.m,j.l,j.xl,j.xxl,j.xxxl,j.beizhu from jincang_t j join kucun_t k on j.kucunid=k.id where k.kuanhao=#{kuanhao} and k.yanse=#{yanse}")
     List<Map> findByKuanhaoYanse(String kuanhao,String yanse);
 }
