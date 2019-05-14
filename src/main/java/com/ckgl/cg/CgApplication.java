ca76d4977df5be5ff381a1ee2c631b7a9280125d
phantomSnail <710312387@qq.com>
2019-05-14 16:24:55 +0800

    不想改了

--- src/main/java/com/ckgl/cg/CgApplication.java
+++ src/main/java/com/ckgl/cg/CgApplication.java
@@ -3,13 +3,20 @@ package com.ckgl.cg;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @MapperScan("com.ckgl.cg.dao")
 @SpringBootApplication
-public class CgApplication {
+public class CgApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CgApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CgApplication.class);
     }
 
 }
