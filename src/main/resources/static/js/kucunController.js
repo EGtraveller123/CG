a87069f118311b3b66079ba400dc11899c69f565
phantomSnail <710312387@qq.com>
2019-05-16 00:50:50 +0800

    不想改了

--- src/main/resources/static/js/kucunController.js
+++ src/main/resources/static/js/kucunController.js
@@ -100,8 +100,6 @@ function goodsListInit() {
                 method : 'GET',
                 queryParams : queryParams,
                 sidePagination : "server",
-                offset : 0,
-                limit : 5,
                 contentType: "application/x-www-form-urlencoded",
                 dataType : 'json',
                 pagination : true,
